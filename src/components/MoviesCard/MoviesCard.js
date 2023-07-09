bca3d0f585bf4a4e3cbdd3f490420d06c16a011e
Vasiliy Lavrov <vaseklavrik@mail.ru>
2023-07-09 12:01:09 +0300

    Feature: add search

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,12 @@
+import "./MoviesCard.css";
+import React from "react";
+
+function MoviesCard() {
+  return (
+    <article className="card">
+
+    </article>
+  );
+}
+
+export default MoviesCard;
---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,13 @@
+import "./MoviesCardList.css";
+import React from "react";
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+function MoviesCardList() {
+  return (
+    <section className="cards">
+        <MoviesCard />
+    </section>
+  );
+}
+
+export default MoviesCardList;
